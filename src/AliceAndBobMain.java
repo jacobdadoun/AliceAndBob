c65b7b087575bb36406f80ddd764e43dd09e9dab
jacobdadoun <jacobdadoun@gmail.com>
2020-11-24 16:35:30 -0500

    "Understanding the basics of thread"

--- src/AliceAndBobMain.java
+++ src/AliceAndBobMain.java
@@ -1,5 +1,31 @@
 public class AliceAndBobMain {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Runnable runner = new HelloWorld();
+
+        // Thread gets passed an object with a run() method to execute, and a string for its name.
+        Thread firstThread = new Thread(runner, "Thread 1");
+        Thread secondThread = new Thread(runner, "Thread 2");
+
+        // .start() has the thread execute the - overridden - run method
+        firstThread.start();
+        secondThread.start();
+
+        // .join() means that the main thread will wait for these two threads to finish.
+        firstThread.join();
+        secondThread.join();
+
+    }
+}
+
+class HelloWorld implements Runnable{
+
+    @Override
+    public void run() {
+        Thread t = Thread.currentThread();
+        System.out.println("Hello World " + t.getName());
+        System.out.println("Bye World " + t.getName());
 
     }
 }
